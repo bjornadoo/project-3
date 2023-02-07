8ac9119f2485367576178adef54ced57266405ed
NickCraft9288 <nickpauel@gmail.com>
2023-02-07 13:36:35 +0100

    fixed navbar + subnav

---
+++ home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+</head>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Document</title>
+<body>
+
+<div class="navbar">
+  <a href="home.php">Home</a>
+  <div class="subnav">
+    <button class="subnavbtn">Bjorn <i class="fa fa-caret-down"></i></button>
+    <div class="subnav-content">
+      <a href="../project-3/bjorn/bjorn.html">Company</a>
+      <a href="#team">Team</a>
+      <a href="#careers">Careers</a>
+    </div>
+  </div> 
+  <div class="subnav">
+    <button class="subnavbtn">Nick <i class="fa fa-caret-down"></i></button>
+    <div class="subnav-content">
+      <a href="../project-3/nick/Nick.php">Bring</a>
+      <a href="#deliver">Deliver</a>
+      <a href="#package">Package</a>
+      <a href="#express">Express</a>
+    </div>
+  </div> 
+  <div class="subnav">
+    <button class="subnavbtn">abdifitah <i class="fa fa-caret-down"></i></button>
+    <div class="subnav-content">
+      <a href="../project-3/abdifatah/abdifatah.html">Link 1</a>
+      <a href="#link2">Link 2</a>
+      <a href="#link3">Link 3</a>
+      <a href="#link4">Link 4</a>
+    </div>
+  </div>
+</div>
+
+<div style="padding:0 16px">
+  <h3>Subnav/dropdown menu inside a Navigation Bar</h3>
+  <p>Hover over the "about", "services" or "partners" link to see the sub navigation menu.</p>
+</div>
+
+</body>
+</html>
