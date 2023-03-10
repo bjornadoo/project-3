023364889416b97ebda883c520d71b36f2542076
bjornadoo <112869722+bjornadoo@users.noreply.github.com>
2023-03-10 12:33:53 +0100

    contact button en cleaner gemaakt

--- website/slide.js
+++ website/slide.js
@@ -78,3 +78,10 @@ var startLoop = setInterval(function() {
     bannerLoop();
 }, 4000);
 }
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
